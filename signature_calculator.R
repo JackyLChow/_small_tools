139ef46048520240caa9b35df7ff628a1aa72385
Jacky Chow <41753000+JackyLChow@users.noreply.github.com>
2023-02-24 08:24:57 -0500

    Update signature_calculator.R

--- signature_calculator.R
+++ signature_calculator.R
@@ -1,8 +1,21 @@
 # signature scores
 ## calculate scores
-sig_scores_ <- list()
-for(i in names(signatures)){
-  sig_s_ <- log2(counts_quantile_normalized[signatures[[i]], metadata$sequencingID] + 1)
-  sig_scores_[[i]] <- colMeans(sig_s_)
-  rm(sig_s_, i)
+signature_calculator <- function(signature_list, expression_table){
+  sig_scores <- list()
+  for(i in names(signature_list)){
+    genes <- signature_list[[i]]
+    if(sum(genes %in% rownames(expression_table)) > 1){
+      sig_scores[[i]] <- colMeans(expression_table[genes[genes %in% rownames(expression_table)], ])
+    }
+    if(sum(genes %in% rownames(expression_table)) == 1){
+      sig_scores[[i]] <- expression_table[genes[genes %in% rownames(expression_table)], ]
+    }
+    if(sum(genes %in% rownames(expression_table)) == 0){
+      cat(paste0("genes in ", i, " signature not present in expression table\n"))
+    }
+    rm(i)
+  }
+  sig_scores <- data.frame(bind_cols(sig_scores))
+  rownames(sig_scores) <- colnames(expression_table)
+  return(sig_scores)
 }
