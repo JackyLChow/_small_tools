07ce93102861b3d31217d8c6cfb8de45d4e6c9a8
Jacky Chow <41753000+JackyLChow@users.noreply.github.com>
2023-03-29 13:18:27 -0400

    Update batch_effect.R

--- test/batch_effect.R
+++ test/batch_effect.R
@@ -3,6 +3,33 @@ meta <- readRDS("~/Documents/BFX_proj/_data_public/Chow_PNAS_2020/Chow_PNAS_meta
 
 counts_batched <- counts
 counts_batched[, 3:8] <- counts_batched[, 3:8] + 15
+batch <- factor(c("A", "A", rep("B", 6), rep("A", 8)))
+
+# normal
+design_ <- model.matrix(~ meta$treatment)
+l_ <- lmFit(counts, design_)
+e_ <- eBayes(l_)
+deg_1 <- topTable(e_, n = Inf)
+
+# artificial batch w/ adjustment to model
+design_ <- model.matrix(~ meta$treatment + batch)
+l_ <- lmFit(counts_batched, design_)
+e_ <- eBayes(l_)
+deg_2 <- topTable(e_, n = Inf)
+
+# artificial batch w/ batch correction
+design_ <- model.matrix(~ meta$treatment)
+counts_corrected <- removeBatchEffect(counts_batched, batch = batch, design = design_)
+l_ <- lmFit(counts_corrected, design_)
+e_ <- eBayes(l_)
+deg_3 <- topTable(e_, n = Inf)
+
+
+
+
+
+counts_batched <- counts
+counts_batched[, 3:8] <- counts_batched[, 3:8] + 15
 counts_batched[, 9:10] <- counts_batched[, 9:10] + 20
 
 batch <- factor(c("A", "A", rep("B", 6), "C", "C", rep("A", 6)))
