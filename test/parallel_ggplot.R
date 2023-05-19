cde4063937ae7ac6342b840824e989f2036b25d2
Jacky Chow <41753000+JackyLChow@users.noreply.github.com>
2023-05-19 14:03:16 -0400

    update

--- test/parallel_ggplot.R
+++ test/parallel_ggplot.R
@@ -1,15 +1,53 @@
 library(ggplot2)
+library(parallel)
 library(foreach)
-library(Cairo)
+
 
 for(i in 1){
-  jpeg(paste0("~/Documents/BFX_proj/_small_tools/test/parallel_ggplot/", i, ".jpg"), height = 300, width = 300, type = "cairo", res = 150)
-  print(
-    ggplot(iris, aes(Sepal.Length, Sepal.Width)) +
-      geom_point() +
-      ggtitle(i)
-  )
+  jpeg(paste0("~/Documents/BFX_proj/_small_tools/test/parallel_ggplot/", i, ".jpg"), height = 300, width = 300, res = 150)
+  ggplot(iris, aes(Sepal.Length, Sepal.Width)) +
+    geom_point() +
+    ggtitle(i)
   dev.off()
 }
 
+system.time(
+  for(i in 1:10){
+    jpeg(paste0("~/Documents/BFX_proj/_small_tools/test/parallel_ggplot/", i, ".jpg"), height = 300, width = 300, res = 150)
+    print(
+      ggplot(iris, aes(Sepal.Length, Sepal.Width)) +
+        geom_point() +
+        ggtitle(i) 
+    )
+    dev.off()
+  }
+)
 
+plots <- list()
+system.time(
+  for(i in 1:1000){
+    plots[[i]] <- ggplot(iris, aes(Sepal.Length, Sepal.Width)) +
+      geom_point() +
+      ggtitle(i)
+  }
+)
+
+system.time(
+  plots <- mclapply(1:1000,
+                    function(i){
+                      ggplot(iris, aes(Sepal.Length, Sepal.Width)) +
+                        geom_point() +
+                        ggtitle(i)
+                    },
+                    mc.cores = 10
+  )
+)
+
+system.time(
+  for(i in 1:length(plots)){
+    jpeg(paste0("~/Documents/BFX_proj/_small_tools/test/parallel_ggplot/", i, ".jpg"), height = 300, width = 300, res = 150)
+    print(
+      plots[[i]]
+    )
+  }
+)
