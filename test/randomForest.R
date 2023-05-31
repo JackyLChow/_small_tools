badaa01f753fcdbaf12aa42156662cb682ad3da9
Jacky Chow <41753000+JackyLChow@users.noreply.github.com>
2023-05-31 13:20:16 -0400

    Update randomForest.R

--- test/randomForest.R
+++ test/randomForest.R
@@ -25,21 +25,31 @@ rf_default <- train(Survived~.,
                     trControl = trControl)
 
 ### simpler iris test
-data(iris)
-TrainData <- iris[,1:4]
-TrainClasses <- iris[,5]
+library(randomForest)
+library(tree)
+library(ggplot2)
+library(GGally)
+library(dplyr)
 
-knnFit1 <- train(TrainData, TrainClasses,
-                 method = "knn",
-                 preProcess = c("center", "scale"),
-                 tuneLength = 10,
-                 trControl = trainControl(method = "cv"))
+index_row <- sample(2,
+                    nrow(iris), 
+                    replace = T, 
+                    prob = c(0.7, 0.3)
+                    )
 
+train_data <- iris[index_row == 1, ]
+test_data <- iris[index_row == 2, ]
 
+iris_classifier <- randomForest(Species ~., 
+                                data = train_data, #train data set 
+                                importance = T)
 
+plot(iris_classifier)
 
+importance(iris_classifier)   #Petal features are more important
 
+qplot(Petal.Width, Petal.Length, data = iris, color = Species)
 
-
-
+predicted_table <- predict(iris_classifier, test_data[,-5])
+table(observed = test_data[,5], predicted = predicted_table)
 
