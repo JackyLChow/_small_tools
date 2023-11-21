25b23a638365f79c88113baffc282fcce144f867
Jacky Chow <41753000+JackyLChow@users.noreply.github.com>
2023-11-21 09:56:37 -0500

    Update signature_calculator.R

--- signature_calculator.R
+++ signature_calculator.R
@@ -1,5 +1,7 @@
-signature_calculator <- function(signature_list, # list has names as signature names; signature is character vector
-                                 counts = tpm){
+library(preprocessCore)
+
+signature_calculator <- function(signature_list, # list has names as signature names; signature is character vector of genes
+                                 counts = log2_tpm_plus_one){
   qnt_ <- normalize.quantiles.robust(counts, use.median = T, keep.names = T, n.remove = 0, remove.extreme = "none") # quantile normalize
   qnt_ <- log2(qnt_ + 1) # log2 transform
   
@@ -7,6 +9,9 @@ signature_calculator <- function(signature_list, # list has names as signature n
   for(i in names(signature_list)){
     genes <- signature_list[[i]]
     missing_genes <- genes[!genes %in% rownames(qnt_)]
+    if(length(missing_genes == length(genes)){
+      cat(paste("all genes missing for", i, "\n"))
+    }
     if(length(missing_genes) > 0){
       cat(paste("missing genes for ", i, ":", missing_genes, "\n"))
     }
@@ -20,35 +25,9 @@ signature_calculator <- function(signature_list, # list has names as signature n
     if(length(genes) == 1){
       sig_scores[[i]] <- qnt_[genes, ]
     }
-    if(length(genes) == 0){
-    }
     rm(i)
   }
   sig_scores <- data.frame(bind_cols(sig_scores))
   rownames(sig_scores) <- colnames(qnt_)
   return(sig_scores)
 }
-
-# # signature scores
-# ## calculate scores
-# signature_calculator <- function(signature_list, # list has names as signature names; signature is character vector
-#                                  expression_table){ # expression table has gene names as rows
-#   sig_scores <- list()
-#   for(i in names(signature_list)){
-#     genes <- signature_list[[i]]
-#     missing_genes <- genes[!genes %in% rownames(expression_table)]
-#     if(sum(genes %in% rownames(expression_table)) > 1){
-#       sig_scores[[i]] <- colMeans(expression_table[genes[genes %in% rownames(expression_table)], ])
-#     }
-#     if(sum(genes %in% rownames(expression_table)) == 1){
-#       sig_scores[[i]] <- expression_table[genes[genes %in% rownames(expression_table)], ]
-#     }
-#     if(sum(genes %in% rownames(expression_table)) == 0){
-#       cat(paste0("genes in ", i, " signature not present in expression table\n"))
-#     }
-#     rm(i)
-#   }
-#   sig_scores <- data.frame(bind_cols(sig_scores))
-#   rownames(sig_scores) <- colnames(expression_table)
-#   return(sig_scores)
-# }
